1c3c8fec30011faad40be51485db9a3e3b357dc5
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-06-30 22:37:16 -0300

    Exibição de um quizz completo

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const urlAPI =("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes");
+let respostasEmbaralhadas 
+
 function createQuizz(){
     alert("deu boa:)")
 }
 
 function pegarQuizzesAxios(){
-    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    const promise = axios.get(urlAPI);
     promise.then(renderizarTodosOsQuizzes);
     promise.catch(() => console.log("deu ruim"));
 }
@@ -22,3 +25,53 @@ function renderizarTodosOsQuizzes(response){
     }
 }
 pegarQuizzesAxios();
+// função get API QUIZZ ID e muda da tela 1 para 2 // 
+function playQuizz(id){
+    const promise = axios.get(`${urlAPI}/${id}`);
+    promise.then(playQuizzId);
+
+    const tela1 = document.querySelector(".tela-1");
+    const tela3 = document.querySelector(".tela-3");
+
+    tela1.classList.add("none");
+    tela3.classList.add("none");
+
+}
+//Função renderizar QuizzClicado//
+function playQuizzId(response){
+
+    const bannerQuizz = document.querySelector(".banner");
+    bannerQuizz.innerHTML += `<div class="black-gradient"></div> 
+                              <img src="${response.data.image}" alt="imagem do quizz clicado">
+                              <h2>${response.data.title}</h2>`
+
+    let idQuizz = response.data;
+    console.log(idQuizz);
+    let questions = response.data.questions;
+    const quizzQuestions = document.querySelector(".levelContainer");
+    console.log(questions);
+    let answersBox="";
+    
+    for(let i=0; i<questions.length; i++ ){
+        //embaralhar respostas //
+        let answerArray = questions[i].answers;
+        let answerArrayEmbaralhado = answerArray.sort(random);
+        // colocar respostas ja embaralhadas//
+        for(let j=0 ;j <questions[i].answers.length; j++ ){
+
+            answersBox +=`<div class="answerBox">
+                                        <img src=${questions[i].answers[j].image}" alt="imagem da pergunta">
+                                        <div class="answer">${questions[i].answers[j].text}</div>
+                                    </div>` 
+        }
+        // colocar quantidade de questoes//
+        quizzQuestions.innerHTML += `<div class="level content">
+        <div class="question content">${questions[i].title}</div>
+        <div class="answersContainer">${answersBox}</div>`
+        
+        answersBox="";
+    }
+}
+function random() {
+    return Math.random() - 0.5;
+  }
